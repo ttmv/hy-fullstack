1d93138ed11f9c1b7e210f38a7e449c82aa47a93
ttmv <tmalmivi@cs.helsinki.fi>
2018-01-17 23:40:43 +0200

    t2.4

--- osa2_kurssit/src/index.js
+++ osa2_kurssit/src/index.js
@@ -38,36 +38,50 @@ const Kurssi = ({ kurssi }) => {
 }
 
 const App = () => {
-  const kurssi = {
-    nimi: 'Half Stack -sovelluskehitys',
-    osat: [
-      {
-        nimi: 'Reactin perusteet',
-        tehtavia: 10,
-        id: 1
-      },
-      {
-        nimi: 'Tiedonvälitys propseilla',
-        tehtavia: 7,
-        id: 2
-      },
-      {
-        nimi: 'Komponenttien tila',
-        tehtavia: 14,
-        id: 3
-      },
-      {
-        nimi: 'Redux',
-        tehtavia: 7,
-        id: 4
-      }
-
-    ]
-  }
+  const kurssit = [
+    {
+      nimi: 'Half Stack -sovelluskehitys',
+      id: 1,
+      osat: [
+        {
+          nimi: 'Reactin perusteet',
+          tehtavia: 10,
+          id: 1
+        },
+        {
+          nimi: 'Tiedonvälitys propseilla',
+          tehtavia: 7,
+          id: 2
+        },
+        {
+          nimi: 'Komponenttien tila',
+          tehtavia: 14,
+          id: 3
+        }
+      ]
+    },
+    {
+      nimi: 'Node.js',
+      id: 2,
+      osat: [
+        {
+          nimi: 'Routing',
+          tehtavia: 3,
+          id: 1
+        },
+        {
+          nimi: 'Middlewaret',
+          tehtavia: 7,
+          id: 2
+        }
+      ]
+    }
+  ]
 
   return (
     <div>
-      <Kurssi kurssi={kurssi} />
+      <Otsikko nimi="Opetusohjelma" />
+      {kurssit.map(kurssi=> <Kurssi key={kurssi.id} kurssi={kurssi} />)}
     </div>
   )
 }
