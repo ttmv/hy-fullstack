befeb24ed86c771b10adc978a5b96e8a1928c2b4
Titti M <tmalmivi@cs.helsinki.fi>
2018-01-19 12:41:32 +0200

    t2.10 + korjauksia aiempiin

--- osa2_puhelinluettelo/src/App.js
+++ osa2_puhelinluettelo/src/App.js
@@ -1,5 +1,33 @@
 import React from 'react';
 
+const Form = (props) => 
+  <form onSubmit={props.handleSubmit}>
+    <div>
+      <label htmlFor="name">Nimi</label> 
+      <input 
+        type="text" id="name" 
+        value={props.newName} 
+        onChange={props.handleNameChange}/>
+      <br />
+      <label htmlFor="num">Numero</label>  
+      <input 
+        type="text" id="num" 
+        value={props.newNum} 
+        onChange={props.handleNumChange}/>
+    </div>
+    <div>
+    <button type="submit">lisää</button>
+  </div>
+</form>
+
+const Header = ({text}) => <h2>{text}</h2>
+
+const Persons = ({persons}) => 
+  <div>
+    {persons.map(p => <p key={p.id}>{p.name} {p.number}</p>)}
+  </div>
+
+
 class App extends React.Component {
   constructor(props) {
     super(props)
@@ -26,7 +54,7 @@ class App extends React.Component {
     }
 
     const persons = this.state.persons.concat(personObj)
-    this.setState({persons, newName: ''})    
+    this.setState({persons, newName: '', newNum: ''})    
   }
 
   handleNameChange = (event) => {
@@ -40,31 +68,17 @@ class App extends React.Component {
   render() {
     return (
       <div>
-        <h2>Puhelinluettelo</h2>
-        <form onSubmit={this.addPerson}>
-          <div>
-            <label htmlFor="name">Nimi</label> 
-            <input 
-              type="text" id="name" 
-              value={this.state.newName} 
-              onChange={this.handleNameChange}/>
-            <br />
-            <label htmlFor="num">Numero</label>  
-            <input 
-              type="text" id="num" 
-              value={this.state.newNum} 
-              onChange={this.handleNumChange}/>
-
-          </div>
-          <div>
-            <button type="submit">lisää</button>
-          </div>
-        </form>
-        <h2>Numerot</h2>
-        <div>
-          {this.state.persons.map(p => <p key={p.id}>{p.name} {p.number}</p>)}
-        </div>
-      
+        <Header text="Puhelinluettelo" />
+        <Header text="Lisää uusi" />
+        <Form 
+          handleSubmit={this.addPerson} 
+          handleNameChange={this.handleNameChange} 
+          handleNumChange={this.handleNumChange}
+          newName={this.state.newName}
+          newNum={this.state.newNum}
+        />
+        <Header text="Numerot" />
+        <Persons persons={this.state.persons} />      
       </div>
     )
   }
