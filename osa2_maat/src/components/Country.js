e8f87fa5e2c0ff6db414a71f60c39ce8c94100bf
ttmv <tmalmivi@cs.helsinki.fi>
2018-01-21 05:15:34 +0200

    lisäyksiä

--- osa2_maat/src/components/Country.js
+++ osa2_maat/src/components/Country.js
@@ -1,17 +1,25 @@
 import React from 'react'
 
 const Country = ({country}) => {
-    //console.log(country)
-    const altTxt = "flag of " + country.name
-    return (
-      <div>
-        name: {country.name} <br/>
-        population: {country.population} <br/>
-        <img src={country.flag} alt={altTxt}/>
-      </div>
-    )
-  }
+  //console.log(country)
+  const altTxt = "flag of " + country.name
+  return (
+    <div>
+      <table>
+        <tbody>
+          <tr><td>name:</td><td>{country.name}</td></tr>
+          <tr><td>native name:</td><td>{country.nativeName}</td></tr>
+          <tr><td>capital:</td><td>{country.capital}</td></tr>
+          <tr><td>population:</td><td>{country.population}</td></tr>
+          <tr><td>region:</td><td>{country.region}</td></tr>
+          <tr><td>subregion:</td><td>{country.subregion}</td></tr>
+        </tbody>
+      </table>
+      <img src={country.flag} alt={altTxt}/>
+    </div>
+  )
+}
 
-  export default Country
+export default Country
 
 
