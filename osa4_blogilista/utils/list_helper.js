a51b2bb47bb447a886519233912ba81e65fe00fd
Titti M <tmalmivi@cs.helsinki.fi>
2018-02-05 15:02:48 +0200

    t4.4

--- osa4_blogilista/utils/list_helper.js
+++ osa4_blogilista/utils/list_helper.js
@@ -2,6 +2,16 @@ const dummy = (blogs) => {
   return 1
 }
 
+const totalLikes = (blogs) => {
+  const reducer = (sum, item) => {
+    return sum+item
+  }
+
+  const likes = blogs.map(blog => blog.likes)
+  return likes.reduce(reducer, 0)
+}
+
 module.exports = {
-  dummy
+  dummy, 
+  totalLikes
 }
