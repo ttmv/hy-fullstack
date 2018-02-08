1690276f960b93b63c5e65a951b6be562b930c1f
ttmv <tmalmivi@cs.helsinki.fi>
2018-02-09 00:57:56 +0200

    t4.9

--- osa4_blogilista/index.js
+++ osa4_blogilista/index.js
@@ -7,24 +7,13 @@ const mongoose = require('mongoose')
 const blogsRouter = require('./controllers/blogs')
 const config = require('./utils/config')
 
-/*
-if ( process.env.NODE_ENV !== 'production' ) {
-  require('dotenv').config()
-}
-*/
-//const mongoUrl = process.env.MONGODB_URI
+
 mongoose.connect(config.mongoUrl)
 mongoose.Promise = global.Promise
 
 app.use(cors())
 app.use(bodyParser.json())
 app.use('/api/blogs', blogsRouter)
-
-//const PORT = 3003
-//const PORT = config.port
-//app.listen(PORT, () => {
-//  console.log(`Server running on port ${PORT}`)
-//})
 
 const server = http.createServer(app)
 
--- osa4_blogilista/tests/blog_api.test.js
+++ osa4_blogilista/tests/blog_api.test.js
@@ -43,6 +43,24 @@ test('all blogs are returned', async () => {
   expect(resp.body.length).toBe(initialBlogs.length)
 })
 
+test('a new blog can be added', async () => {
+  const newBlog = {
+    title: "Type wars",
+    author: "Robert C. Martin",
+    url: "http://blog.cleancoder.com/uncle-bob/2016/05/01/TypeWars.html"
+  }
+
+  await api
+    .post('/api/blogs')
+    .send(newBlog)
+    .expect(201)
+    .expect('Content-Type', /application\/json/)
+  
+  const resp = await api.get('/api/blogs')
+  expect(resp.body.length).toBe(initialBlogs.length + 1)
+  const blogTitles = resp.body.map(b => b.title)
+  expect(blogTitles).toContain("Type wars")
+})
 
 afterAll(() => {
   server.close()
