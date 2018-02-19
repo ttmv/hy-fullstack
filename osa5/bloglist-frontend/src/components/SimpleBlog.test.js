fb3cf52713fd84b927cf4e6c35f5b89a2a6c8be7
Titti M <tmalmivi@cs.helsinki.fi>
2018-02-19 15:41:29 +0200

    t5.13

--- osa5/bloglist-frontend/src/components/SimpleBlog.test.js
+++ osa5/bloglist-frontend/src/components/SimpleBlog.test.js
@@ -18,4 +18,20 @@ describe('<SimpleBlog />', () => {
     expect(infoDiv.text()).toContain(blog.title)
     expect(likesDiv.text()).toContain(blog.likes)
   })
+
+  it('clicking like twice calls event handler twice', () => {
+    const blog = {
+      author: "testiauthor",
+      title: "testititle",
+      likes: 5
+    }
+
+    const mockHandler = jest.fn()
+    const blogComponent = shallow(<SimpleBlog blog={blog} onClick={mockHandler}/>)
+    const button = blogComponent.find('button')
+    button.simulate('click')
+    button.simulate('click')
+
+    expect(mockHandler.mock.calls.length).toBe(2)
+  })
 })
